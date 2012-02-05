2364f07780a7bcf836c6fa6eddcc58a273d0356d
Luke Mcildoon <luke@Lukes-MacBook-Air.local>
2012-02-05 17:19:15 +1100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MediamedoiV2::Application.config.secret_token = 'b817a78986b8cd0f32bab078ddc9e01e1c9fcc85318e959b2d36898ae9fbbf12c248465ef8f003b2aea771d207201f9562f5874240ccf480a121fa9b80b8b385'
